ec4bd2e7bdecc7b7a6d1536ae5d7b30bc77a6a5b
fabiopasilva1 <fabiopasilva1@gmail.com>
2024-11-25 09:42:51 -0300

    clear: remove files and clean code server

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const DB = require("./src/config/database");
 
 const V1 = require("./src/routes/v1/indexRoute");
 const app = express();
@@ -7,28 +6,6 @@ const app = express();
 app.use(express.json());
 
 app.use("/", V1);
-app.get("/lista/:page?/:limit?", async (req, res) => {
-    const query = req.query;
-    const params = req.params;
-
-    console.log({ query });
-    try {
-        const db = await DB.connect(
-            "target_credlinks",
-            {
-                ...query,
-            },
-            { ...params }
-        );
-
-        res.status(200).json(db);
-    } catch (erro) {
-        res.json(500).json(erro);
-    }
-});
-// Rota para consulta dinâmica com campos específicos para regex
-
-// app.post("/consulta", ConsultaRoute);
 
 process.on("uncaughtException", function (err) {
     console.log("Caught exception: " + err);
