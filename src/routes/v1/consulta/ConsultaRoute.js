6e396f91b1856c8702b886d7a5c827d7e223ea0d
fabiopasilva1 <fabiopasilva1@gmail.com>
2024-12-03 19:44:41 -0300

    chore: updates

--- src/routes/v1/consulta/ConsultaRoute.js
+++ src/routes/v1/consulta/ConsultaRoute.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const {
-  ConsultaController,
+    ConsultaController,
 } = require("../../../controllers/ConsultaController");
 const router = express.Router();
 
