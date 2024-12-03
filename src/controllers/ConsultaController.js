ade38190999120c066c75cf97b94193dce464899
fabiopasilva1 <fabiopasilva1@gmail.com>
2024-12-03 19:44:24 -0300

    clear: remove consolelog

--- src/controllers/ConsultaController.js
+++ src/controllers/ConsultaController.js
@@ -1,14 +1,15 @@
 const ConsultaService = require("../services/ConsultaService");
 
 class ConsultaController {
-  static async find(req, res) {
-    try {
-      const response = await ConsultaService.find(req, res);
-      if (response) res.status(200).json(response);
-    } catch (error) {
-      res.status(500).json({ error: "Erro ao realizar a consulta." });
+    static async find(req, res) {
+        try {
+            const response = await ConsultaService.find(req, res);
+            if (response) res.status(200).json(response);
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ error: "Erro ao realizar a consulta." });
+        }
     }
-  }
 }
 
 module.exports = { ConsultaController };
